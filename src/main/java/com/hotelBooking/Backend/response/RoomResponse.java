7f09f459500e98c79cc0da14db510e4745a174bb
Harshita <harshitasinghal28@gmail.com>
2025-09-09 14:34:02 +0530

    Initial commit of backend code

---
+++ src/main/java/com/hotelBooking/Backend/response/RoomResponse.java
@@ -0,0 +1,34 @@
+package com.hotelBooking.Backend.response;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.commons.codec.binary.Base64;
+
+import java.util.*;
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+public class RoomResponse {
+    private Long id;
+    private String roomType;
+    private BigDecimal roomPrice;
+    private boolean isBooked;
+    private String photo;
+    private List<BookingResponse> bookings;
+
+    public RoomResponse(Long id, String roomType, BigDecimal roomPrice) {
+        this.id = id;
+        this.roomType = roomType;
+        this.roomPrice = roomPrice;
+    }
+
+    public RoomResponse(Long id, String roomType, BigDecimal roomPrice, boolean isBooked, byte[] photoBytes) {
+        this.id = id;
+        this.roomType = roomType;
+        this.roomPrice = roomPrice;
+        this.isBooked = isBooked;
+        this.photo = photoBytes != null ? Base64.encodeBase64String(photoBytes) : null;
+//        this.bookings = bookings;
+    }
+}
---
+++ src/main/java/com/hotelBooking/Backend/service/BookingService.java
@@ -0,0 +1,13 @@
+package com.hotelBooking.Backend.service;
+
+import com.hotelBooking.Backend.model.BookedRoom;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookingService {
+    public List<BookedRoom> getAllBookingsByRoomId(Long roomId) {
+        return null;
+    }
+}
