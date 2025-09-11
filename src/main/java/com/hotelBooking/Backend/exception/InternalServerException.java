e0230f67f0ffda2eccf48f2b6d6271a291b42cc4
Harshita <harshitasinghal28@gmail.com>
2025-09-11 16:49:36 +0530

    view existing room

--- src/main/java/com/hotelBooking/Backend/exception/InternalServerException.java
+++
@@ -1,7 +0,0 @@
-package com.hotelBooking.Backend.exception;
-
-public class InternalServerException extends RuntimeException {
-	public InternalServerException(String message) {
-		super(message);
-	}
-}
