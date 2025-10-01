b00cf4edc9ce2177bd061ee87fd99796425c8541
palak garg <gargpalak0401@gmail.com>
2025-10-01 12:09:49 +0530

    Fully Completed project

--- Backend/src/main/java/com/hotelBooking/Backend/exception/InvalidBookingRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.hotelBooking.Backend.exception;
-
-public class InvalidBookingRequestException extends RuntimeException {
-    public InvalidBookingRequestException(String message) {
-        super(message);
-    }
-}
--- Backend/src/main/java/com/hotelBooking/Backend/service/BookedRoomService.java
+++
@@ -1,7 +0,0 @@
-package com.hotelBooking.Backend.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class BookedRoomService {
-}
